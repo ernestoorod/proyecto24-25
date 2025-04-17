b3db2d165cd8a053892ea72c39ee4f270c6f17de
Ernesto Rodríguez <ernessttooo16@gmail.com>
2025-04-17 12:37:38 +0200

    segundo commit

---
+++ src/main/java/es/proyecto/Proyecto2425Application.java
@@ -0,0 +1,13 @@
+package es.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto2425Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto2425Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/proyecto/Proyecto2425ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto2425ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
